0464a4a29c72e588439b05e392ac30b46b929878
Bruno Bulacia <beueleceia@gmail.com>
2025-09-28 20:08:44 -0400

    Exportacion a SpringBoot

--- src/api/exports.ts
+++ src/api/exports.ts
@@ -1,7 +1,7 @@
 import axios from "../lib/axios";
 
-export const getZip = async () => {
-  const response = await axios.get("export/demo-zip", {
+export const getZip = async (diagramId: string) => {
+  const response = await axios.get(`export/generate-spring/${diagramId}`, {
     responseType: "blob",
   });
   return response.data;
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -66,7 +66,7 @@ export function AppSidebar() {
   }
 
   const handleExportSpringBoot = async () => {
-    const res = await getZip();
+    const res = await getZip(currentDiagramId!);
     const url = window.URL.createObjectURL(new Blob([res]));
     const link = document.createElement('a');
     link.href = url;
